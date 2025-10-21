2e6e2498dc4f1e456fdbac4dc92b41ba81e27948
Monday David Osakpawan <rynstheoverlord@gmail.com>
2025-10-21 15:51:05 +0100

    added request/fetch feature

--- main.go
+++ main.go
@@ -4,12 +4,22 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/table"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"golang.org/x/mod/modfile"
 )
+
+type packageListItem struct {
+	title string
+	desc  string
+}
+
+func (i packageListItem) Title() string       { return i.title }
+func (i packageListItem) Description() string { return i.desc }
+func (i packageListItem) FilterValue() string { return i.title }
 
 type gopackage struct {
 	path       string
@@ -35,14 +45,20 @@ type model struct {
 	err          error
 	install      bool
 	installInput textinput.Model
+	packageList  list.Model
 }
 
 func initialModel() model {
+	go fetchPackagesFromIndex()
+
 	var installTI = textinput.New()
 	installTI.CharLimit = -1
 	installTI.Focus()
 	installTI.Placeholder = "github.com/..."
-	return model{installInput: installTI}
+
+	var packList = list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0)
+	packList.Title = "Available packages"
+	return model{installInput: installTI, packageList: packList}
 }
 
 func (m model) Init() tea.Cmd {
@@ -56,8 +72,23 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c", "q":
 			return m, tea.Quit
 
-		case "i":
+		case "ctrl+g":
 			m.install = !m.install
+
+		case "esc":
+			if m.install {
+				m.install = false
+			}
+
+		case "tab":
+			if m.install {
+				if m.installInput.Focused() {
+					m.installInput.Blur()
+					m.packageList.Select(0)
+				} else {
+					m.installInput.Focus()
+				}
+			}
 
 		}
 
@@ -72,7 +103,11 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.rendered = true
 	}
 
-	if m.rendered {
+	if m.install {
+		var cmd tea.Cmd
+		m.installInput, cmd = m.installInput.Update(msg)
+		return m, cmd
+	} else if m.rendered {
 		var cmd tea.Cmd
 		m.ptable, cmd = m.ptable.Update(msg)
 		return m, cmd
@@ -105,11 +140,36 @@ func (m model) View() string {
 	parsedString = fmt.Sprintf("%v\n%v", projectVersion, m.ptable.View())
 
 	if m.install {
-		m.installInput.Width = m.window.width / 2
-		parsedString = lipgloss.NewStyle().
-			Align(lipgloss.Center, lipgloss.Center).
+		var filter = filterGolangPackages(m.installInput.Value())
+		var listItems []list.Item
+
+		for _, f := range filter {
+			listItems = append(listItems, packageListItem{title: f.Path})
+		}
+		m.packageList.SetSize(m.window.width-5, m.window.height/2)
+		m.packageList.SetItems(listItems)
+		m.installInput.Width = m.window.width
+		var input = lipgloss.NewStyle().
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("63")).
+			Padding(0, 1).
+			Width(m.window.width - 5).
 			Render(m.installInput.View())
+
+		var output = lipgloss.NewStyle().
+			Width(m.window.width - 5).
+			Border(lipgloss.RoundedBorder()).
+			Align(lipgloss.Center).
+			Render(func() string {
+				if len(filter) > 0 && len(m.installInput.Value()) > 0 {
+					return m.packageList.View()
+				}
+
+				return "No package found!"
+			}())
+		parsedString = fmt.Sprintf("%v\n%v", input, output)
 	}
+
 	return parsedString
 }
 
